cc291684265407cca2229680e204da2c507afd6e
KeemoBTECH <xemail944@gmail.com>
2026-08-29 13:32:37 +0300

    Update CORS origin to include production URL

--- kinangotti-api/server.js
+++ kinangotti-api/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001', 'http://192.168.8.164:3000'] }));
+app.use(cors({ origin: ['https://kinangotvc-ac-ke-o13u.vercel.app/', 'http://localhost:3001', 'http://192.168.8.164:3000'] }));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
